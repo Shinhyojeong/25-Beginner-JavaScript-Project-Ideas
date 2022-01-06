ab047bcf4bb2187cf7b711c3ab7ea888279ead9f
Shinhyojeong <hyojeonge0@naver.com>
2022-01-06 21:39:54 +0900

    ✨ feat : 07 실시간 시계 기능 구현

--- 07-DigitalClock/src/App.js
+++ 07-DigitalClock/src/App.js
@@ -1,6 +1,10 @@
+import Clock from './components/Clock.js'
+
 export default function App({
     targetEl
 }){
-
+    new Clock({
+        targetEl
+    })
 }
 
---
+++ 07-DigitalClock/src/utils/processDayInformation.js
@@ -0,0 +1,24 @@
+export const confirmDay = ( dayNum ) => {
+    if( dayNum === 1 ){ return 'MON' }
+    if( dayNum === 2 ){ return 'TUS' }
+    if( dayNum === 3 ){ return 'WED' }
+    if( dayNum === 4 ){ return 'THU' }
+    if( dayNum === 5 ){ return 'FRI' }
+    if( dayNum === 6 ){ return 'SAT' }
+    if( dayNum === 7 ){ return 'SUN' }
+}
+
+export const addZero = ( time ) => {
+    if( 10 > time ){ return `0${time}`}
+    return time
+}
+
+export const confirmAmPm = ( hours ) => {
+    if( hours > 12 ){ return 'PM'}
+    return 'AM'
+}
+
+export const readHours = (hours) => {
+    if(hours > 12) { return addZero(hours - 12) }
+    return addZero(hours)
+}
