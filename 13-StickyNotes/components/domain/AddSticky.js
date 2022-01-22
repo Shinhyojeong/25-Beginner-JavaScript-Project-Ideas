5632f15f6df5b9b23251049b0bb474e1b40797f5
Shinhyojeong <hyojeonge0@naver.com>
2022-01-23 00:23:23 +0900

    ✨ feat : 13 AddSticky 함수 구현

---
+++ 13-StickyNotes/components/domain/AddSticky.js
@@ -0,0 +1,38 @@
+import Modal from '../atomic/Modal.js'
+
+export default function AddSticky({
+    targetEl,
+    initialState,
+    onClose,
+    onSubmit,
+}){
+    this.state = initialState
+
+    this.setState = nextState => {
+        this.state = nextState
+        addStickyModal.setState(
+            this.state
+        )
+    }
+
+    const addStickyModal = new Modal({
+        targetEl,
+        initialState : {
+            visible : this.state.modalVisible,
+            className : 'add-sticky-modal' ,
+            content : '<textarea class="add-sticky-textarea" placeholder="내용을 입력해 주세요"/>'
+        },
+        onClose: () => {
+            onClose()
+        }
+    })
+
+    const addTextArea = document.querySelector('.add-sticky-textarea')
+    addTextArea.addEventListener('keyup', e => {
+        if(e.key !== 'Enter'){ return }
+
+        const { target } = e
+        onSubmit(target.value)
+        target.value = ''
+    })
+}
