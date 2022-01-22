787452a2325fa195a3722aea733d4c5035e3210c
Shinhyojeong <hyojeonge0@naver.com>
2022-01-23 01:36:58 +0900

    ✨ feat : StickyNoteList 함수 구현

---
+++ 13-StickyNotes/components/domain/StickyNoteList.js
@@ -0,0 +1,39 @@
+import StickyNote from './StickyNote.js'
+
+export default function StickyNoteList({
+    targetEl,
+    initialState,
+    onRemove,
+}){
+    this.state = initialState
+
+    const stickyNoteListEl = document.createElement('div')
+    stickyNoteListEl.className = 'sticky-note-list'
+
+    this.setState = nextState => {
+        this.state = nextState
+        this.render()
+    }
+
+    this.render = () => {
+        stickyNoteListEl.innerHTML = ''
+
+        this.state?.forEach((sticky) => {
+            new StickyNote({
+                targetEl : stickyNoteListEl,
+                initialState : sticky,
+            })
+        })
+    }
+
+    targetEl.append(stickyNoteListEl)
+
+    stickyNoteListEl.addEventListener('dblclick', e => {
+        const stickyNote = e.target.closest('div')
+        const { id } = stickyNote.dataset
+
+        if(!id) { return }
+
+        onRemove(id * 1)
+    })
+}
