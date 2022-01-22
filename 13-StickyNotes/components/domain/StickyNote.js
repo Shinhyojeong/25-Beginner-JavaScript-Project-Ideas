787af39ca34502342798a25e95a8434565d029da
Shinhyojeong <hyojeonge0@naver.com>
2022-01-23 01:36:41 +0900

    ✨ feat : StickyNote 함수 구현

---
+++ 13-StickyNotes/components/domain/StickyNote.js
@@ -0,0 +1,23 @@
+import Text from '../atomic/Text.js'
+
+export default function StickyNote({
+    targetEl,
+    initialState,
+}){
+    this.state = initialState
+
+    const stickyNoteEl = document.createElement('div')
+    stickyNoteEl.className = 'sticky-note'
+    stickyNoteEl.setAttribute('data-id', this.state.id)
+
+    new Text({
+        targetEl : stickyNoteEl,
+        initialState : {
+            containerType : 'span',
+            content : this.state.note
+        }
+    })
+
+    targetEl.append(stickyNoteEl)
+
+}
