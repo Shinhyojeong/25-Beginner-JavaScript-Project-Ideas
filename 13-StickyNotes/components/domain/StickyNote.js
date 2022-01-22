4c247cff283ef68e3ce2804cbfed069ed9ff14c7
Shinhyojeong <hyojeonge0@naver.com>
2022-01-23 01:44:24 +0900

    ✨ feat : Text 함수에 className 넘겨주기

--- 13-StickyNotes/components/domain/StickyNote.js
+++ 13-StickyNotes/components/domain/StickyNote.js
@@ -14,6 +14,7 @@ export default function StickyNote({
         targetEl : stickyNoteEl,
         initialState : {
             containerType : 'span',
+            className : 'sticky-note-text',
             content : this.state.note
         }
     })
