b1833c80ebd2aec262b1fc506aae5255017f3f0f
Shinhyojeong <hyojeonge0@naver.com>
2022-01-24 09:36:40 +0900

    ✨ feat : 15 Button 함수 구현

---
+++ 15-Math/src/components/atomic/Button.js
@@ -0,0 +1,27 @@
+export default function Button({
+    targetEl,
+    initialState,
+    onClick
+}){
+    this.state = initialState
+
+    const btnEl = document.createElement('div')
+    btnEl.className = this.state.className
+
+    this.render = () => {
+        const { value, content } = this.state
+
+        btnEl.innerHTML = `
+            <button value='${value}'>${content}</button>
+        `
+    }
+
+    this.render()
+    targetEl.append(btnEl)
+
+    btnEl.addEventListener('click', e => {
+        if(!e.target.value){ return }
+
+        onClick(e)
+    })
+}
