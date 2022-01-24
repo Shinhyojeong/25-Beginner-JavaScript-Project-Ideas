23d5780eee7d71aef0d841f4c6cbab28f0fe3595
Shinhyojeong <hyojeonge0@naver.com>
2022-01-24 16:17:56 +0900

    ✨ feat : 15 랜덤 숫자 식 생성하는 util 구현

---
+++ 15-Math/src/utils/makeRandomNum.js
@@ -0,0 +1,31 @@
+const MAX_NUMBER = 10
+const MIN_NUMBER = 1
+
+export const makeRandomNum = () => Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER)
+
+export const derivingCorrectAnswer = (operator) => {
+    const firstNum = makeRandomNum()
+    const secondNum = makeRandomNum()
+
+    const correctAnswer =  parseFloat(eval(`${firstNum} ${operator} ${secondNum}`).toFixed(1))
+
+    return correctAnswer < 0 ? derivingCorrectAnswer(operator) : [ firstNum, secondNum, correctAnswer ]
+}
+
+export const randomAnswerArray = (correctAnswer, operator) => {
+    const answerList = [ correctAnswer, derivingCorrectAnswer(operator)[2], derivingCorrectAnswer(operator)[2] ]
+
+    return answerList.sort(() => Math.random() - 0.5)
+}
+
+export const randomState = (operator) => {
+    const nextState = derivingCorrectAnswer(operator)
+
+    return {
+        operator,
+        firstNum : nextState[0],
+        secondNum : nextState[1],
+        correctAnswer : nextState[2],
+        answerList : randomAnswerArray(nextState[2], operator),
+    }
+}
