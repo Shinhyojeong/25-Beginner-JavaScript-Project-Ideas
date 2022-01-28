20f80d41faf785b5d2f329a618e97b7ebd6feeec
Shinhyojeong <hyojeonge0@naver.com>
2022-01-28 09:40:56 +0900

    ✨ feat : 16 Input 함수 구현

---
+++ 16-UnsplashAPI/src/atomic/Input.js
@@ -0,0 +1,16 @@
+import { createElement } from '../utils/createElement.js'
+
+export default function Input({ targetEl, initialState, onKeyUp }) {
+  this.state = initialState
+
+  const { elClassName, elPlaceholder } = this.state
+
+  const inputEl = createElement('input', elClassName)
+  inputEl.setAttribute('placeholder', elPlaceholder)
+
+  targetEl.append(inputEl)
+
+  inputEl.addEventListener('keyup', (e) => {
+    onKeyUp(e)
+  })
+}
