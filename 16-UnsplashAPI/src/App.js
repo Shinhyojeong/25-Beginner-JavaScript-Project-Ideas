0c493081cbb5ef5e751c8f8e995ee2d3ee873b17
Shinhyojeong <hyojeonge0@naver.com>
2022-01-28 16:07:15 +0900

    ✨ feat : 16 App 함수에서 데이터 관리

--- 16-UnsplashAPI/src/App.js
+++ 16-UnsplashAPI/src/App.js
@@ -1,1 +1,31 @@
-export default function App({ targetEl }) {}
+import { Header, ReadImageList } from './components/domain/index.js'
+import { fetchUnsplashApi } from './utils/handleUnsplashData.js'
+
+export default function App({ targetEl }) {
+  this.state = {
+    searchTerm: '',
+    searchResult: '',
+  }
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    readImageList.setState(this.state.searchResult)
+  }
+
+  new Header({
+    targetEl,
+    onSubmit: async (searchTerm) => {
+      const searchResult = await fetchUnsplashApi(searchTerm)
+      this.setState({
+        ...this.state,
+        searchTerm,
+        searchResult,
+      })
+    },
+  })
+
+  const readImageList = new ReadImageList({
+    targetEl,
+    initialState: {},
+  })
+}
