2de9a6d67469fe8d14e272a5f329586fb97e4dd5
Shinhyojeong <hyojeonge0@naver.com>
2022-01-31 23:13:41 +0900

    ✨ feat : 17 App 에서 quote 불러와서 상태로 관리

--- 17-Typewriter/src/App.js
+++ 17-Typewriter/src/App.js
@@ -1,1 +1,31 @@
-export default function App({ targetEl }) {}
+import TypingQuote from './components/TypingQuote.js'
+import { request } from './api/api.js'
+import { fetchQuote } from './utils/fetchQuote.js'
+
+export default function App({ targetEl }) {
+  this.state = {
+    quote:
+      'Always be a first-rate version of yourself, instead of a second-rate version of somebody else',
+  }
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    typingQuote.setState(this.state)
+  }
+
+  const typingQuote = new TypingQuote({
+    targetEl,
+    initialState: this.state,
+    nextQuote: async () => {
+      const data = await fetchQuote()
+      console.log(data.content)
+
+      this.setState({
+        ...this.state,
+        quote: data.content,
+      })
+    },
+  })
+
+  fetchInitialState()
+}
