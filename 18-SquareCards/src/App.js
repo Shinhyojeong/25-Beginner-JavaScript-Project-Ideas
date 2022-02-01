f9b69927fcaafccc182995b2d668ac0c08bf018b
Shinhyojeong <hyojeonge0@naver.com>
2022-02-01 22:53:04 +0900

    ✨ feat : 18 App 에서 상태 가져와서 하위로 전달

--- 18-SquareCards/src/App.js
+++ 18-SquareCards/src/App.js
@@ -1,1 +1,14 @@
-export default function App({ targetEl }) {}
+import Card from './components/domain/Card.js'
+import CardList from './components/domain/CardList.js'
+import { DUMMY_CARD_LIST } from './data/dummy/CardList.js'
+
+export default function App({ targetEl }) {
+  this.state = {
+    cardList: DUMMY_CARD_LIST,
+  }
+
+  new CardList({
+    targetEl,
+    initialState: this.state.cardList,
+  })
+}
