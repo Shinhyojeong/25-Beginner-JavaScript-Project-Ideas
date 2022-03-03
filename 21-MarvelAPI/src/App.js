e7ad5d8a450b22b014893b6111cbb69edc8d4d05
Shinhyojeong <hyojeonge0@naver.com>
2022-03-03 23:35:16 +0900

    ✨ feat : 21 App에서 Section 생성

--- 21-MarvelAPI/src/App.js
+++ 21-MarvelAPI/src/App.js
@@ -1,1 +1,31 @@
-export default function App({ targetEl }) {}
+import CardList from './components/domain/CardList.js'
+import Header from './components/domain/Header.js'
+import Section from './components/domain/Section.js'
+import { DEFAULT_RESULT } from './data/defaultValue.js'
+
+export default function App({ targetEl }) {
+  new Header({
+    targetEl,
+  })
+  new Section({
+    targetEl,
+    initialState: {
+      elClassName: 'wrapper character',
+      searchList: [
+        {
+          inputPlaceholder: 'CHARACTER NAME...',
+          btnContent: 'SEARCH',
+        },
+      ],
+      cardInfo: {
+        title: 'Character',
+        content:
+          'Fetch the image and ID of your favorite Marvel character. You can use the ID to search for their comic books!',
+        imgUrl: '/21-MarvelAPI/src/assets/image/img_section_character.jpg',
+        imgAlt: 'character-img',
+      },
+      fetchUrl: 'comics?upc=',
+      defaultResult: DEFAULT_RESULT,
+    },
+  })
+}
---
+++ 21-MarvelAPI/src/data/defaultValue.js
@@ -0,0 +1,20 @@
+export const DEFAULT_RESULT = [
+  {
+    title: 'Black Panther',
+    content: 'ID: 1009187',
+    imgUrl: '/21-MarvelAPI/src/assets/image/img_black_panther.jpg',
+    imgAlt: 'black_panther-img',
+  },
+  {
+    title: 'Thanos',
+    content: 'ID: 1009652',
+    imgUrl: '/21-MarvelAPI/src/assets/image/img_thanos.jpg',
+    imgAlt: 'thanos-img',
+  },
+  {
+    title: 'Doctor Doom',
+    content: 'ID: 1009281',
+    imgUrl: '/21-MarvelAPI/src/assets/image/img_doctor_doom.jpg',
+    imgAlt: 'doctor_doom-img',
+  },
+]
