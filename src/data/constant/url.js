876757f3daf4fa56ae018e9108044dcfeaaad7d0
Shinhyojeong <hyojeonge0@naver.com>
2022-03-23 23:46:26 +0900

    ✨ feat : url 상수화 작업

---
+++ src/data/constant/url.js
@@ -0,0 +1,18 @@
+export const ROUTE_URL = {
+  HEX_COLORS: 'hex-colors',
+  RANDOM_QUOTE_GENERATOR: 'random-quote-generator',
+  THE_MESSAGE: 'the-message',
+  COUNTER: 'counter',
+  IMAGE_CAROUSEL: 'image-carousel',
+  DIGITAL_CLOCK: 'digital-clock',
+  CALCULATOR: 'calculator',
+  GROCERY_LIST: 'grocery-list',
+  TIP_CALCULATOR: 'tip-calculator',
+  FLASHCARD: 'flashcards',
+  TO_DO_LIST: 'to-do-list',
+  STICKY_NOTES: 'sticky-notes',
+  TIMER: 'timer',
+  MATH: 'math',
+  UNSPLASH_API: 'unsplash-api',
+  TYPEWRITER: 'typewriter',
+}
